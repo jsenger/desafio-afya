f30a05cf9c0beec7488d658e5293bda641072b09
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-25 18:35:44 -0400

    Create Navbar component

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import { Navigation } from './styles';
+
+const Navbar: React.FC = () => {
+  return (
+    <Navigation>
+      <img src="/img/afya-logo-white.svg" height="60" alt="Logo Afya" />
+    </Navigation>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/components/Navbar/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Navigation = styled.nav`
+  width: 100%;
+  height: 80px;
+
+  background: var(--main-color);
+`;
