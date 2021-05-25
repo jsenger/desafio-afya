6d90d752839f9fc70984d27ce1ec47240aa90835
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-25 18:12:28 -0400

    Style home page

--- frontend/src/assets/GlobalStyles/index.ts
+++ frontend/src/assets/GlobalStyles/index.ts
@@ -11,4 +11,8 @@ export default createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Montserrat', sans-serif;
   }
-`
+
+  body { 
+    background: #f3eff5ff;
+  }
+`;
--- frontend/src/views/pages/Home/styles.ts
+++ frontend/src/views/pages/Home/styles.ts
@@ -1,7 +1,49 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  margin: auto;
+
+  width: 800px;
+  max-width: 80%;
+  height: 100vh;
+
+  img {
+    width: 100%;
+    margin-bottom: 3rem;
+
+    align-self: flex-end;
+  }
+
   button {
     color: var(--main-color);
+    font-size: 2rem;
+    font-weight: bold;
+
+    height: 3.5rem;
+    width: 43%;
+
+    background: transparent;
+    border: 3px solid var(--main-color);
+    border-radius: 8px;
+
+    align-self: flex-start;
+
+    cursor: pointer;
+
+    transition: ease-in-out 0.5s;
+
+    &:not(:last-child) {
+      margin-right: 3rem;
+    }
+
+    &:hover {
+      color: #fff;
+      background-color: var(--main-color);
+    }
   }
-`
+`;
