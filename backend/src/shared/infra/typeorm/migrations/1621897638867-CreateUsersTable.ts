277eaf988b638345cb3e3f29e6bc521bc4196655
luc0liv <luoliveiradacunha@gmail.com>
2021-05-24 21:11:18 -0300

    Create Tables: Users, Specialists, Clients

---
+++ backend/src/shared/infra/typeorm/migrations/1621897638867-CreateUsersTable.ts
@@ -0,0 +1,49 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsersTable1621897638867 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.createTable(
+            new Table ({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'
+                    },
+
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropTable('users');
+    }
+
+}
