6344b771580c5bc8b466a7d6f3d1a1f67ebdcf72
luc0liv <luoliveiradacunha@gmail.com>
2021-05-29 00:27:05 -0300

    New Sidebar using material ui

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,28 +1,224 @@
-import { SideBarContainer } from "./styles";
-import { Link } from "react-router-dom";
+import clsx from "clsx";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { HiMenu } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import { IconContext } from "react-icons";
+import { useState } from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
-const Sidebar: React.FC = () => {
+const drawerWidth = 300;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: "none",
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      color: "#ecebe4",
+      backgroundColor: "#004AAD",
+    },
+    drawerHeader: {
+      display: "flex",
+      alignItems: "center",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: "space-between",
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  })
+);
+
+interface SideBarProps {
+  title: string;
+}
+
+export default function Sidebar(props: SideBarProps) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <SideBarContainer>
-      <div className="menu-icons">
-        <img src="./img/vit_logo.svg" alt="Vitality logo" width="30%" />
-        <img src="./img/menu-button.svg" alt="Menu icon" width="15%" />
-      </div>
-
-      <div className="menu-links">
-      <hr />
-        <Link to="#">Clientes</Link>
-        <Link to="#">Especialistas</Link>
-        <Link to="#">Atendimentos</Link>
-        <Link to="#">Prontuários</Link>
-        <hr />
-      </div>
-
-      <div className="logout">
-        <Link to="#">Logout</Link>
-      </div>
-    </SideBarContainer>
+    <div>
+      <AppBar
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+        position="fixed"
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+          >
+            <HiMenu />
+          </IconButton>
+          {props.title}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <div className={classes.drawerHeader}>
+          <img src="./img/vit_logo.svg" alt="Vitality logo" width="100px" />
+          <IconButton onClick={handleDrawerClose}>
+            <IconContext.Provider value={{ color: "#ECEBE4" }}>
+              <AiOutlineClose />
+            </IconContext.Provider>
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {["Clientes", "Especialistas", "Atendimentos", "Prontuários"].map(
+            (text, index) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            )
+          )}
+        </List>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemText primary={"Logout"} />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </p>
+        <p>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </p>
+      </main>
+    </div>
   );
-};
+}
 
-export default Sidebar;
+// import { SideBarContainer } from "./styles";
+// import { Link } from "react-router-dom";
+// import { useState } from "react";
+// import Drawer from "@material-ui/core/Drawer";
+
+// const Sidebar: React.FC = () => {
+//   const [open, setOpen] = useState(false);
+
+//   const handleDrawerOpen = () => setOpen(true);
+
+//   const handleDrawerClose = () => setOpen(false);
+
+//   return (
+//     <Drawer variant="persistent" anchor="left" open={open}>
+//       <SideBarContainer>
+//         <div className="menu-icons">
+//           <img src="./img/vit_logo.svg" alt="Vitality logo" width="30%" />
+//           <img src="./img/menu-button.svg" alt="Menu icon" width="15%" />
+//         </div>
+
+//         <div className="menu-links">
+//           <hr />
+//           <Link to="#">Clientes</Link>
+//           <Link to="#">Especialistas</Link>
+//           <Link to="#">Atendimentos</Link>
+//           <Link to="#">Prontuários</Link>
+//           <hr />
+//         </div>
+
+//         <div className="logout">
+//           <Link to="#">Logout</Link>
+//         </div>
+//       </SideBarContainer>
+//     </Drawer>
+//   );
+// };
+
+// export default Sidebar;
