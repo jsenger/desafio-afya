25d88ed168fd78e32d2285dd7727e00c8545cdee
jéssica senger <jessicasbogoni1@gmail.com>
2021-06-05 12:44:37 -0400

    Error message when search has no results

--- frontend/src/components/Search/index.tsx
+++ frontend/src/components/Search/index.tsx
@@ -1,4 +1,11 @@
-import { Dispatch, FormEvent, SetStateAction, useCallback, useState } from 'react';
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useCallback,
+  useState,
+} from 'react';
+import Swal from 'sweetalert2';
 import { search } from '../../services/search';
 import { SearchField } from './styles';
 
@@ -11,14 +18,26 @@ interface SearchProps {
 export default function Search({ title, endpoint, setResult }: SearchProps) {
   const [searchParams, setSearchParams] = useState<string>('');
 
-  const searchSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const searchSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
 
-    const response = await search({ endpoint, params: searchParams });
-    
-    setResult(response.data);
-    
-  }, [endpoint, searchParams, setResult]);
+      const response = await search({ endpoint, params: searchParams });
+
+      if (response.data.length) {
+        setResult(response.data);
+      } else {
+        Swal.fire({
+          title: 'Ops!',
+          text: 'Nenhum resultado encontrado.',
+          icon: 'error',
+          confirmButtonText: 'Fechar',
+          confirmButtonColor: '#ff312e',
+        });
+      }
+    },
+    [endpoint, searchParams, setResult]
+  );
 
   return (
     <SearchField onSubmit={searchSubmit}>
